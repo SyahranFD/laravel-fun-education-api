ca12bf0ffdfa73f39f7ed6ead1d7ff75bbc5a0a1
SyahranFD <fadhilrafa1@gmail.com>
2024-05-31 08:01:31 +0700

    feat: [SF] add setup notification firebase

---
+++ app/Notifications/PushNotification.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Fcm\FcmChannel;
+use NotificationChannels\Fcm\FcmMessage;
+use NotificationChannels\Fcm\Resources\Notification as FcmNotification;
+
+
+class PushNotification extends Notification
+{
+    public function via($notifiable)
+    {
+        return [FcmChannel::class];
+    }
+
+    public function toFcm($notifiable): FcmMessage
+    {
+        return (new FcmMessage(notification: new FcmNotification(
+                title: 'Account Activated',
+                body: 'Your account has been activated.',
+                image: 'http://example.com/url-to-image-here.png'
+            )))
+            ->data(['data1' => 'value', 'data2' => 'value2'])
+            ->custom([
+                'android' => [
+                    'notification' => [
+                        'color' => '#0A0A0A',
+                    ],
+                    'fcm_options' => [
+                        'analytics_label' => 'analytics',
+                    ],
+                ],
+                'apns' => [
+                    'fcm_options' => [
+                        'analytics_label' => 'analytics',
+                    ],
+                ],
+            ]);
+    }
+}
