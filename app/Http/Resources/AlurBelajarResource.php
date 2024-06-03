071904930b51694a18d45ed2564ca10e8e3e48f6
SyahranFD <fadhilrafa1@gmail.com>
2024-06-03 21:18:42 +0700

    fix: [SF] fix validation in resource alur belajar

--- app/Http/Resources/AlurBelajarResource.php
+++ app/Http/Resources/AlurBelajarResource.php
@@ -14,13 +14,17 @@ class AlurBelajarResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $tahap_a = $this->tahap === 'A' ? true : false;
+        $tahap_b = $this->tahap === 'B' ? true : false;
+        $tahap_c = $this->tahap === 'C' ? true : false;
+
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
             'tahap' => $this->tahap,
-            'tahap_a' => $this->tahap === 'A' ? true : false,
-            'tahap_b' => $this->tahap === 'B' ? true : false,
-            'tahap_c' => $this->tahap === 'C' ? true : false,
+            'tahap_a' => $tahap_a || $tahap_b || $tahap_c,
+            'tahap_b' => $tahap_b || $tahap_c,
+            'tahap_c' => $tahap_c,
         ];
     }
 }
