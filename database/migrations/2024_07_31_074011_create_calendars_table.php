b298c30f14dfc2a2b5e0fac64e2b14006f388f2a
SyahranFD <fadhilrafa1@gmail.com>
2024-07-31 21:06:02 +0700

    feat: [SF] add calendar migration and model

---
+++ database/migrations/2024_07_31_074011_create_calendars_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('calendars', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('title');
+            $table->text('description');
+            $table->string('date');
+            $table->string('color')->default('#FFFFFF');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('calendars');
+    }
+};
