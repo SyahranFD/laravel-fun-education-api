37a1b97e171d6627f0c9ab57ec58d31026fb1815
SyahranFD <fadhilrafa1@gmail.com>
2024-08-09 14:13:53 +0700

    feat: [SF] add notification in pengajuan tabungan

--- app/Http/Controllers/SavingApplicationController.php
+++ app/Http/Controllers/SavingApplicationController.php
@@ -7,15 +7,18 @@ use App\Http\Resources\SavingApplicationResource;
 use App\Models\Saving;
 use App\Models\SavingApplication;
 use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Laravel\Firebase\Facades\Firebase;
 
 class SavingApplicationController extends Controller
 {
     public function store(Request $request)
     {
         $request->validate(['category' => 'required|string',]);
-        auth()->user();
+        $user = auth()->user();
 
         $savingApplicationData = $request->all();
         $savingApplicationData['status'] = 'Pending';
@@ -27,7 +30,13 @@ class SavingApplicationController extends Controller
         $savingApplication = SavingApplication::create($savingApplicationData);
         $savingApplication = new SavingApplicationResource($savingApplication);
 
-        return $this->resStoreData($savingApplication);
+        $admin = User::where('role', 'admin')->first();
+        $notification = 'User tidak memiliki fcm_token atau fcm_token tidak valid';
+        if ($admin->fcm_token) {
+            $notification = $this->notification($admin, 'Pengajuan Tabungan Baru' , 'Terdapat pengajuan tabungan '. strtolower($savingApplication->category) .' dari '. strtolower($user->nickname) .', harap segera diperiksa', $user->id, '/detail-saving-page');
+        }
+
+        return $this->resStoreData($savingApplication, $notification);
     }
 
     public function index()
@@ -96,7 +105,18 @@ class SavingApplicationController extends Controller
             $saving->update(['saving' => $saving->saving - 100000,]);
         }
 
-        return new SavingApplicationResource($savingApplication);
+        if ($request->status == 'Accepted' || $request->status == 'Rejected') {
+            $status = $request->status == 'Accepted' ? 'Diterima' : 'Ditolak';
+            $user = User::find($savingApplication->user_id);
+            $notification = 'User tidak memiliki fcm_token atau fcm_token tidak valid';
+            if ($user->fcm_token) {
+                $notification = $this->notification($user, 'Pengajuan Tabungan '. $status, 'Pengajuan tabungan '. strtolower($savingApplication->category) .' anda '. strtolower($status) .', silahkan cek status pengajuan', $user->id, '/saving-page');
+            }
+        }
+
+        $savingApplication = new SavingApplicationResource($savingApplication);
+
+        return $this->resStoreData($savingApplication, $notification);
     }
 
     public function destroy($id)
@@ -111,5 +131,23 @@ class SavingApplicationController extends Controller
         $savingApplication->delete();
 
         return $this->resDataDeleted('Saving Application');
+    }
+
+    public function notification($user, $title, $body, $user_id, $route)
+    {
+        $FcmToken = $user->fcm_token;
+        $message = CloudMessage::fromArray([
+            'token' => $FcmToken,
+            'notification' => [
+                'title' => $title,
+                'body' => $body,
+            ],
+        ])->withData([
+            'route' => $route,
+            'user_id' => $user_id,
+        ]);
+
+        Firebase::messaging()->send($message);
+        return $message;
     }
 }
