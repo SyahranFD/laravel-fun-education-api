2c18fd88f26d6491055e5593cc6c18882b78ce5a
SyahranFD <fadhilrafa1@gmail.com>
2024-08-19 07:43:41 +0700

    feat: [SF] add send gmail to verify email

--- app/Http/Controllers/OtpController.php
+++ app/Http/Controllers/OtpController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\OtpRequest;
 use App\Http\Resources\OtpResource;
+use App\Mail\VerifyEmail;
 use App\Models\Otp;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 
 class OtpController extends Controller
@@ -19,7 +21,10 @@ class OtpController extends Controller
             $otpData['id'] = 'otp-'.Str::uuid();
         } while (Otp::where('id', $otpData['id'])->exists());
 
+        Otp::where('email', $request->email)->delete();
         $otp = Otp::create($otpData);
+
+        Mail::to($request->email)->send(new VerifyEmail($otp));
 
         return $this->resStoreData($otp);
     }
@@ -29,9 +34,7 @@ class OtpController extends Controller
         $otp = Otp::where('email', auth()->user()->email)->latest()->first();
         if ($otp->otp == $request->otp) {
             $otp->delete();
-            return response()->json([
-                'message' => 'OTP is valid'
-            ]);
+            return response(['message' => 'OTP is valid']);
         }
 
         return response(['message' => 'OTP is invalid']);
