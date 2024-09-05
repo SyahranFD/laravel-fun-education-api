d38d4cdc4e5fdd59e9165bf1823f4a4c7ef88e6c
SyahranFD <fadhilrafa1@gmail.com>
2024-09-05 10:05:29 +0700

    feat: [SF] add school information migration and model

---
+++ app/Models/SchoolInformationDesc.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SchoolInformationDesc extends Model
+{
+    use HasFactory;
+
+    public $incrementing = false;
+
+    protected $guarded = [];
+
+    public function schoolInformation()
+    {
+        return $this->belongsTo(SchoolInformation::class);
+    }
+}
---
+++ database/migrations/2024_09_05_095958_create_school_information_descs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('school_information_descs', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('school_information_id');
+            $table->string('desc');
+            $table->timestamps();
+
+            $table->foreign('school_information_id')->references('id')->on('school_information')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('school_information_descs');
+    }
+};
